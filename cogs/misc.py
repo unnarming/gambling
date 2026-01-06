3151453b8b3d2b27f495976d135452deb2ac1a47
unnarming <unnarming@gmail.com>
2026-01-06 08:22:44 -0500

    Import EventsCog into MiscCog

--- cogs/misc.py
+++ cogs/misc.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 from embeds import Embeds
 from config import Config
 from sql import Sql
+from cogs.events import EventsCog
 
 class MiscCog(commands.Cog):
     def __init__(self, bot: commands.Bot, config: Config, sql: Sql):
